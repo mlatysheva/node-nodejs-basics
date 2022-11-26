cd2f15e1a4fb77606621c14870687933183989df
mlatysheva <mlatysheva@gmail.com>
2022-11-26 13:42:28 +0100

    feat: add list and read functions

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import * as fs from 'fs/promises';
+import { doesExist } from '../utils/doesExist.js';
+import { getResolvedPath } from '../utils/getResolvedPath.js';
+import { throwErrorMessage } from '../utils/throwErrorMessage.js';
+
 const list = async () => {
-    // Write your code here 
+  const filesFolderExists = await doesExist(getResolvedPath('fs/files'));
+  try {
+    if (filesFolderExists) {
+      await (await fs.readdir(getResolvedPath('fs/files'), { withFileTypes: true }))
+        .forEach((file) => {
+          console.log(file.name);
+      });
+    } else {
+      throwErrorMessage();
+    }
+  } catch(err) {
+    console.error(err);
+  }
 };
 
 await list();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import * as fs from 'fs/promises';
+import { getResolvedPath } from '../utils/getResolvedPath.js';
+import { throwErrorMessage } from '../utils/throwErrorMessage.js';
+
+const fileToRead = 'fs/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await fs.readFile(getResolvedPath(fileToRead), 'utf8');
+    console.log(content);
+  } catch(err) {
+    throwErrorMessage();
+  }
 };
 
 await read();
