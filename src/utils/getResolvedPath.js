4791b7a867d7235571c61c24adad4c5e7ca9b52d
mlatysheva <mlatysheva@gmail.com>
2022-11-29 23:30:15 +0100

    docs: update readme

--- src/utils/getResolvedPath.js
+++ src/utils/getResolvedPath.js
@@ -1,4 +1,4 @@
-import path, {dirname} from 'node:path';
+import path, { dirname } from 'node:path';
 
 export const getResolvedPath = (_filename, ...paths) => {
   const _dirname = dirname(_filename);
